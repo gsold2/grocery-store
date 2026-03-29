c2490e451ab7a26c52b3656b6a3efd71eb10eb88
grigorii <gsold2@gmail.com>
2026-03-29 17:08:09 +0300

    init service-catalog module

---
+++ service-catalog/src/test/java/com/github/gsold2/catalog/ServiceApplicationTests.java
@@ -0,0 +1,12 @@
+package com.github.gsold2.catalog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class ServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
